b5133e42746fa6f571da5ebbb7919cac7e3d9342
Adith <adithwidyapradipta@gmail.com>
2017-08-24 17:02:04 +0700

    React-Redux middleware

---
+++ react-redux-middleware/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+	return [
+		{ title: 'JavaScript The Good Parts', pages: 109, author: 'Budhi Priyatmojo' },
+		{ title: 'Harry Potter and The Sorcerer Stone', pages: 60, author: 'Fajar Karim' },
+		{ title: 'The Dark Tower', pages: 70, author: 'Patrick Benz' },
+		{ title: 'Eloquent Ruby', pages: 10, author: 'Sukmo Ronggo' }
+	]
+}
